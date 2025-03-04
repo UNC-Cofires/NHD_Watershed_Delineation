c319f62ca32857c91a524a16b30c9acbb43f8750
Kieran Patrick Fitzmaurice (kieranf) <kpf10@pitt.edu>
2025-03-04 16:18:55 -0500

    updated job submission script

--- delineate_watershed.sh
+++ delineate_watershed.sh
@@ -6,10 +6,10 @@
 #SBATCH --mem=64g
 #SBATCH -t 1-00:00:00
 #SBATCH --mail-type=all
-#SBATCH --job-name=flow_network
+#SBATCH --job-name=watershed_delineation
 #SBATCH --mail-user=kieranf@email.unc.edu
 export PYTHONWARNINGS="ignore"
 module purge
 module load anaconda/2023.03
-conda activate /proj/characklab/projects/kieranf/flood_damage_index/fli-env-v1
+conda activate nhd-env-v1
 python3.12 delineate_watershed.py
